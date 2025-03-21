7c513ade9b8e0e29194b79a2ee64f83fc24618a8
伍侃 <wu-kan@163.com>
2025-03-21 16:51:59 +0800

    发现网页是动态的，改成更灵活的写法

--- tests/hook.py
+++ tests/hook.py
@@ -6,6 +6,8 @@
 直接hook请求函数，返回解密后的内容
 
 首页是静态网页，翻页是fetch请求
+
+发现js文件名也是动态变化的
 """
 import asyncio
 
@@ -13,9 +15,33 @@ from query_table import launch_browser
 
 
 def __hook(x, y, obj):
-    print(x)
-    print(y)
-    print(obj)
+    if x == '/sun/ranking/fundRankV3':
+        print(x)
+        print(y)
+        print(obj)
+
+
+async def on_route(route, request):
+    """找到特殊js文件，每过一段时间特殊文件名不同，但内部的函数名不变"""
+    response = await route.fetch()
+    body = await response.text()
+    if "uXpFetch" not in body:
+        await route.fulfill(response=response)
+        return
+
+    # 网页中引用的js文件，会变化
+    # <script type="module" src="/_nuxt3/BFem2fS2.js" crossorigin></script>
+    print(request.url)
+    body = body.replace("export{", """
+var _hook = uXpFetch;
+uXpFetch =async function(e,t) {
+const ret=_hook(e,t);
+ret.then((r)=>{window.__hook(e,t,r)});
+return ret;
+};
+export{""")
+
+    await route.fulfill(content_type="text/javascript; charset=utf-8", body=body)
 
 
 async def main() -> None:
@@ -23,7 +49,7 @@ async def main() -> None:
     print(browser.is_connected(), page.is_closed())
 
     await page.expose_function("__hook", __hook)
-    await page.route("**/_nuxt3/rY-FUFEX.js", lambda route: route.fulfill(path="rY-FUFEX.js"))  # 第3行
+    await page.route("**/_nuxt3/*.js", on_route)
     await page.goto("https://dc.simuwang.com/smph", wait_until="load")
 
     # 强行翻页，产生fetch请求
@@ -42,3 +68,14 @@ async def main() -> None:
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+"""
+<script type="module" src="/_nuxt3/BFem2fS2.js" crossorigin></script>
+
+var _hook = uXpFetch;
+uXpFetch =async function(e,t) {
+    const ret=_hook(e,t);
+    ret.then((r)=>{window.__hook(e,t,r)});
+    return ret;
+};
+"""
